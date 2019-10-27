c203769ece274a228c1b52b9cc11011f346d5670
BurningSmile <burningsmile10@gmail.com>
2019-10-26 18:25:39 -0600

    Updated redshift toggle script to look for status code of 2 for running.

--- i3/scripts/Redshift-toggle.sh
+++ i3/scripts/Redshift-toggle.sh
@@ -4,12 +4,16 @@
 
 # check if process is running ( 0 = off, 1 = on )
 STATUS="$(ps -ef | grep -w '[r]edshift' | wc -l)"
+echo "${STATUS}"
 # if off then turn on
 if [[ "${STATUS}" == 0 ]]; then
 #  notify-send -t 1 --icon=info "RedShift" "On"
   redshift >/dev/null 2>&1 & disown
 # else if on then turn off
 elif [[ "${STATUS}" == 1 ]]; then
+#  notify-send -t 1 --icon=info "RedShift" "Off"
+  redshift -x && killall redshift
+elif [[ "${STATUS}" == 2 ]]; then
 #  notify-send -t 1 --icon=info "RedShift" "Off"
   redshift -x && killall redshift
 fi
